3148f140c146181bc5014df08867c658c0bec115
Fernando Quinonez <fernando.andres.quinonez.granados@gmail.com>
2016-01-30 23:14:42 -0500

    bower and grunt integration

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+// Project configuration.
+grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+  uglify: {
+    options: {
+      banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    },
+    build: {
+      src: 'src/BBT.js',
+      dest: 'build/BBT.min.js'
+    }
+  }
+});
+
